2ace7a85df65275521cffee2fb7e057661ebc078
Jonathan Tran <tranjt7@uci.edu>
2024-05-31 15:16:02 -0700

    Implemented all 3 graph algorithms.

--- graph.py
+++ graph.py
@@ -2,6 +2,7 @@
 # each file that uses a graph should import it from this file.
 
 from collections.abc import Iterable
+from collections import deque, defaultdict
 
 class Graph:
 	def __init__(self, num_nodes: int, edges: Iterable[tuple[int, int]]):
@@ -36,6 +37,57 @@ class Graph:
 	def get_neighbors(self, node: int) -> Iterable[int]:
 		return self.adj_list[node]
 
+	def get_degree(self, node):
+		if node in self.adj_list:
+			return len(self.adj_list[node])
+		else:
+			return 0
+		
+	def get_farthest_node_d(self, node):
+		visited = set()
+		queue = deque([(node, 0)])
+		farthest_node = node
+		max_dist = 0
+		while queue:
+			curr_node, dist = queue.popleft()
+			visited.add(curr_node)
+			for neighbor in self.adj_list[curr_node]:
+				if neighbor not in visited:
+					visited.add(neighbor)
+					queue.append((neighbor, dist + 1))
+					if dist + 1 > max_dist:
+						max_dist = dist + 1
+						farthest_node = neighbor
+		return farthest_node, max_dist
+	
+	def get_degeneracy_order(self):
+		L = []
+		L_set = set()  # Track nodes in L.
+		n = self.get_num_nodes()
+		dv = {v: len(neighbors) for v,neighbors in self.adj_list.items()}
+		D = defaultdict(list)
+		for v, degree in dv.items():
+			D[degree].append(v)
+		Nv = defaultdict(list)
+		k = 0
+		for node in range(n):
+			i = 0
+			while i not in D or not D[i]:
+				i += 1
+			k = max(k, i)
+			v = D[i].pop()
+			L.insert(0, v)
+			L_set.add(v)
+			for w in self.get_neighbors(v):
+				if w not in L_set:
+					old_deg = dv[w]
+					new_deg = old_deg - 1
+					dv[w] = new_deg
+					D[old_deg].remove(w)
+					D[new_deg].append(w)
+					Nv[v].append(w)
+		return L, Nv, k
+	
 	# feel free to define new methods in addition to the above
 	# fill in the definitions of each required member function (above),
 	# and for any additional member functions you define
--- graph_algorithms.py
+++ graph_algorithms.py
@@ -1,14 +1,44 @@
 # explanations for these functions are provided in requirements.py
 
 from graph import Graph
+from collections import defaultdict
+import random
 
 def get_diameter(graph: Graph) -> int:
-	raise NotImplementedError
+	if not graph.adj_list:  # Empty adjacency lists have 0 diameter.
+		return 0
+	r = random.choice(list(graph.adj_list.keys()))
+	max_dist = 0
+	w, dist = graph.get_farthest_node_d(r)
+	while (dist > max_dist):
+		max_dist = dist
+		r = w
+		w, dist = graph.get_farthest_node_d(r)
+	return max_dist
 
 
 def get_clustering_coefficient(graph: Graph) -> float:
-	raise NotImplementedError
+	denom = 0  # Calculate the number of 2-edge paths. 
+	for node in graph.adj_list:
+		node_deg = graph.get_degree(node)
+		c_coeff = (node_deg * (node_deg-1)) / 2  
+		denom += c_coeff
+	triangle_cnt = 0  # Calculate the number of triangles using d-degeneracy.
+	L, Nv, k = graph.get_degeneracy_order()
+	for v in L:
+		n = len(Nv[v])
+		for i in range(n):
+			for j in range(i+1, n):
+				edge = (min(Nv[v][i], Nv[v][j]), max(Nv[v][i], Nv[v][j]))
+				if edge in graph.edges:
+					triangle_cnt += 1
+	C = 3 * triangle_cnt / denom
+	return C
 
 
 def get_degree_distribution(graph: Graph) -> dict[int, int]:
-	raise NotImplementedError
+	deg_dict = defaultdict(int)
+	dv = {v: len(neighbors) for v,neighbors in graph.adj_list.items()}
+	for v in dv:
+		deg_dict[dv[v]] += 1
+	return deg_dict
